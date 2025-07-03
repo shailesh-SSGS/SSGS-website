f472509932e587c6baa4b0dd7488550b8f73bde5
shailesh-SSGS <shailesh.ssglobalservices@gmail.com>
2025-07-03 17:39:40 +0530

    Updated homepage layout and CSS styling

--- js/header.js
+++ js/header.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const headerHTML = `
-    <!-- - - - - - - - - - - - - - Header - - - - - - - - - - - - - - - - -->
-    <header id="header" class="header">
-
-      <!-- pre-header (NOT sticky) -->
+    <header id="header" class="header sticky-header">
+      <!-- pre-header -->
       <div class="pre-header">
         <div class="container">
           <div class="flex-row flex-justify">
             <ul class="contact-info">
-              <li><a href="tel:+91-9999992605"><i class="icon-phone pr-4"> </i> +91-9999992605</a></li>
+              <li><a href="tel:+91-9560935898"><i class="icon-phone pr-4"> </i> +91-9999992605</a></li>
               <li><a href="tel:+91-9718430644"><i class="icon-phone"> </i> +91-9718430644</a></li>
               <li><a href="tel:011-40194697"><i class="icon-phone"> </i> 011-40194697</a></li>
               <li><a href="mailto:contact@ssglobalservices.com"><i class="icon-email"> </i> info@ssglobalservices.in</a></li>
@@ -24,22 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
       </div>
 
-      <!-- Sticky Main Menu -->
-      <div class="menu-holder" style="position: sticky; top: 0; z-index: 1000; background: #fff; box-shadow: 0 2px 5px rgba(0,0,0,0.05);">
+      <!-- main menu -->
+      <div class="menu-holder">
         <div class="menu-wrap">
           <div class="container">
             <div class="nav-item flex-row flex-justify flex-center">
               <div class="logo-wrap">
                 <a href="index.html" class="logo float-start p-0">
-                  <img src="images/logo.webp" style="max-height:90px;" alt="Logo">
+                  <img src="images/logo.webp" style="max-height:90px;" alt="">
                 </a>
               </div>
-
               <nav id="main-navigation" class="main-navigation">
                 <ul id="menu" class="clearfix">
-                  <li class="current"><a href="#">Home</a></li>
-
-                  <li class="dropdown"><p>About</p>
+                  <li><a href="index.html" class="nav-link home-link">Home</a></li>
+                  <li class="dropdown">
+                    <a href="#">About</a>
                     <div class="sub-menu-wrap">
                       <ul>
                         <li><a href="about.html">About Us</a></li>
@@ -49,14 +46,14 @@ document.addEventListener("DOMContentLoaded", function () {
                       </ul>
                     </div>
                   </li>
-
-                  <li class="dropdown has-megamenu"><p>Services</p>
+                  <li class="dropdown has-megamenu">
+                    <a href="#">Services</a>
                     <div class="sub-menu-wrap mega-menu flex-row">
                       <div class="mega-submenu">
-                        <h5 class="mega-title"><span class="cicon-heating"></span>License & Certification</h5>
+                        <h5 class="mega-title">License & Certification</h5>
                         <ul>
                           <li><a href="BIS-domestic-manufacturers.html">BIS (ISI Mark) for Domestic Manufacturers</a></li>
-                          <li><a href="#">BIS (ISI Mark) for Foreign Manufacturers</a></li>
+                          <li><a href="BIS-foreign-manufacturers.html">BIS (ISI Mark) for Foreign Manufacturers</a></li>
                           <li><a href="#">BIS Scheme-X Certification</a></li>
                           <li><a href="#">BIS Certificate of Conformity (CoC)</a></li>
                           <li><a href="#">BIS License For Toys</a></li>
@@ -66,22 +63,23 @@ document.addEventListener("DOMContentLoaded", function () {
                         </ul>
                       </div>
                       <div class="mega-submenu">
-                        <h5 class="mega-title"><span class="cicon-cooling"></span>Registrations</h5>
+                        <h5 class="mega-title">Registrations</h5>
                         <ul>
                           <li><a href="#">BEE Registration</a></li>
                           <li><a href="#">BIS (CRS) Registration For Electronic Product</a></li>
-                          <li><a href="#">E-Waste Management (EPR)</a></li>
+                          <li><a href="#">EPR Registration for E-Waste</a></li>
+                          <li><a href="#">EPR Registration for Plastic Waste</a></li>
+                          <li><a href="#">EPR Registration for Battery Waste</a></li>
+                          <li><a href="#">EPR Registration for Tyre Waste</a></li>
                           <li><a href="#">WPC-ETA Approval</a></li>
+                          <li><a href="#">BIS Scheme-X Certification</a></li>
                           <li><a href="#">TEC/MTCTE Approval</a></li>
                           <li><a href="#">TAC & IMEI Registration</a></li>
-                          <li><a href="#">MSME & NSIC Registration</a></li>
-                          <li><a href="#">LMPC Registration</a></li>
-                          <li><a href="#">Start-Up Registration</a></li>
-                          <li><a href="#">Legal Metrology</a></li>
+                          <li><a href="#">Legal Metrology (LMPC)</a></li>
                         </ul>
                       </div>
                       <div class="mega-submenu">
-                        <h5 class="mega-title"><span class="cicon-plumbing"></span>Auxiliary</h5>
+                        <h5 class="mega-title">Auxiliary</h5>
                         <ul>
                           <li><a href="#">EMI-EMC Test</a></li>
                           <li><a href="#">RF Testing</a></li>
@@ -94,18 +92,19 @@ document.addEventListener("DOMContentLoaded", function () {
                       </div>
                     </div>
                   </li>
-
-                  <li class="dropdown"><p>Latest notification</p>
+                  <li class="dropdown">
+                    <a href="#">Latest notification</a>
                     <div class="sub-menu-wrap">
                       <ul>
                         <li><a href="#">News & Update</a></li>
                         <li><a href="#">Upcoming Events</a></li>
+                        <li><a href="blogs.html">Blogs</a></li>
                         <li><a href="#">Career</a></li>
                       </ul>
                     </div>
                   </li>
-
-                  <li class="dropdown"><p>Gallery</p>
+                  <li class="dropdown">
+                    <a href="#">Gallery</a>
                     <div class="sub-menu-wrap">
                       <ul>
                         <li><a href="gallery.html">Image Gallery</a></li>
@@ -114,8 +113,8 @@ document.addEventListener("DOMContentLoaded", function () {
                       </ul>
                     </div>
                   </li>
-
-                  <li class="dropdown"><p>Contact Us</p>
+                  <li class="dropdown">
+                    <a href="#">Contact Us</a>
                     <div class="sub-menu-wrap">
                       <ul>
                         <li><a href="contact.html">Contact Us</a></li>
@@ -123,8 +122,8 @@ document.addEventListener("DOMContentLoaded", function () {
                       </ul>
                     </div>
                   </li>
-
-                  <li class="dropdown"><p>Login</p>
+                  <li class="dropdown">
+                    <a href="#">Login</a>
                     <div class="sub-menu-wrap">
                       <ul>
                         <li><a href="#">Payment</a></li>
@@ -141,7 +140,23 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
       </div>
     </header>
-    <!-- - - - - - - - - - - - - end Header - - - - - - - - - - - - - - - -->
   `;
+
   document.getElementById("header-container").innerHTML = headerHTML;
+
+  const path = window.location.pathname.split("/").pop() || "index.html";
+  const navItems = document.querySelectorAll('#menu li');
+
+  navItems.forEach(parentLi => {
+    const childLink = parentLi.querySelector("a");
+    if (!childLink) return;
+    const href = childLink.getAttribute("href");
+    if (href && path === href) {
+      parentLi.classList.add("current");
+      const grandParent = parentLi.closest(".dropdown");
+      if (grandParent && grandParent.tagName === "LI") {
+        grandParent.classList.add("current");
+      }
+    }
+  });
 });
